e79ae46430ec330e1ad1bda1f5c74559e542bfe0
German Khokhlov <gekh.oklo@gmail.com>
2021-11-17 18:30:27 +0300

    logging

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Body, HttpCode, ParseIntPipe, ValidationPipe } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Patch, Post, Body, HttpCode, ParseIntPipe, ValidationPipe, Logger } from "@nestjs/common";
 import { identity } from "rxjs";
 import { CreateEventDto } from "./create-event.dto";
 import { UpdateEventDto } from "./update-event.dto";
@@ -9,63 +9,70 @@ import { InjectRepository } from "@nestjs/typeorm";
 @Controller('/events')
 export class EventsController {
 
-    constructor(
-        @InjectRepository(Event)
-        private readonly repository: Repository<Event>
-    ) { }
+  private readonly logger = new Logger(EventsController.name)
 
-    @Get()
-    async findAll() {
-        return await this.repository.find()
-    }
+  constructor(
+    @InjectRepository(Event)
+    private readonly repository: Repository<Event>
+  ) { }
 
-    @Get('/practice')
-    async practice() {
-        return await this.repository.find({
-            select: ['id', 'when'],
-            where: [{
-                id: MoreThan(3),
-                when: MoreThan(new Date('2021-02-12T13:00:00'))
-            }, {
-                description: Like('%meet%')
-            }],
-            take: 2,
-            order: {
-                'id': 'DESC',
-            },
-        })
-    }
+  @Get()
+  async findAll() {
+    this.logger.log(`Hit the findAll route.`)
+    const events =  await this.repository.find()
+    this.logger.debug(`Found ${events.length} events.`)
+    this.logger.error(`I'm just kidding. There's no actually error here :)`)
+    this.logger.warn(`I warn you. Do you hear me? I warn you like hell!`)
+    return events
+  }
 
-    @Get(':id')
-    async findOne(@Param('id', ParseIntPipe) id) {
-        console.log(typeof id)
-        return await this.repository.findOne(id)
-    }
+  @Get('/practice')
+  async practice() {
+    return await this.repository.find({
+      select: ['id', 'when'],
+      where: [{
+        id: MoreThan(3),
+        when: MoreThan(new Date('2021-02-12T13:00:00'))
+      }, {
+        description: Like('%meet%')
+      }],
+      take: 2,
+      order: {
+        'id': 'DESC',
+      },
+    })
+  }
 
-    @Post()
-    async create(@Body(new ValidationPipe({ groups: ['create'] })) input: CreateEventDto) {
-        return await this.repository.save({
-            ...input,
-            when: new Date(input.when),
-        })
-    }
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id) {
+    console.log(typeof id)
+    return await this.repository.findOne(id)
+  }
 
-    @Patch(':id')
-    async update(
-        @Param('id') id, 
-        @Body(new ValidationPipe({ groups: ['update'] })) input: UpdateEventDto) {
-        const event = this.repository.findOne(id)
-        return await this.repository.save({
-            ...event,
-            ...input,
-            // when: input.when ? new Date(input.when) : event.when
-        })
-    }
+  @Post()
+  async create(@Body(new ValidationPipe({ groups: ['create'] })) input: CreateEventDto) {
+    return await this.repository.save({
+      ...input,
+      when: new Date(input.when),
+    })
+  }
 
-    @Delete(':id')
-    @HttpCode(204)
-    async remove(@Param('id') id) {
-        // const event = this.repository.findOne(id)
-        await this.repository.delete(id)
-    }
+  @Patch(':id')
+  async update(
+    @Param('id') id,
+    @Body(new ValidationPipe({ groups: ['update'] })) input: UpdateEventDto) {
+    const event = this.repository.findOne(id)
+    return await this.repository.save({
+      ...event,
+      ...input,
+      // when: input.when ? new Date(input.when) : event.when
+    })
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async remove(@Param('id') id) {
+    // const event = this.repository.findOne(id)
+    await this.repository.delete(id)
+  }
 }
