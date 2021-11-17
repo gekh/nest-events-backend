d941edf8551b1733fb8d3805c26d7d836e5e2834
German Khokhlov <gekh.oklo@gmail.com>
2021-11-17 15:07:14 +0300

    TypeORM practice

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -3,7 +3,7 @@ import { identity } from "rxjs";
 import { CreateEventDto } from "./create-event.dto";
 import { UpdateEventDto } from "./update-event.dto";
 import { Event } from "./event.entity";
-import { Repository } from "typeorm";
+import { Like, MoreThan, Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 
 @Controller('/events')
@@ -17,6 +17,23 @@ export class EventsController {
     @Get()
     async findAll() {
         return await this.repository.find()
+    }
+
+    @Get('/practice')
+    async practice() {
+        return await this.repository.find({
+            select: ['id', 'when'],
+            where: [{
+                id: MoreThan(3),
+                when: MoreThan(new Date('2021-02-12T13:00:00'))
+            },{
+                description: Like('%meet%')
+            }],
+            take: 2,
+            order: {
+                'id': 'DESC',
+            },
+        })  
     }
 
     @Get(':id')
@@ -34,18 +51,18 @@ export class EventsController {
 
     @Patch(':id')
     async update(@Param('id') id, @Body() input: UpdateEventDto) {
-        const event:Event = this.repository.findOne(id)
+        const event = this.repository.findOne(id)
         return await this.repository.save({
             ...event,
             ...input,
-            when: input.when ? new Date(input.when) : event.when
+            // when: input.when ? new Date(input.when) : event.when
         })
     }
 
     @Delete(':id')
     @HttpCode(204)
     async remove(@Param('id') id) {
-        const event = this.repository.findOne(id)
-        await this.repository.delete(event)
+        // const event = this.repository.findOne(id)
+        await this.repository.delete(id)
     }
 }
