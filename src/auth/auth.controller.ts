5777f5cf63bfea5896dccc34de86d519be73173f
German Khokhlov <gekh.oklo@gmail.com>
2021-11-18 19:35:14 +0300

    current user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,46 @@
-import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Logger, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
 import { AuthService } from "./auth.service";
+import { CreateUserDto } from "./create-user.dto"
+import { CurrentUser } from "./current-user.decorator"
+import { User } from "./user.entity"
 
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name)
 
   constructor(
     private readonly authService: AuthService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
   ){}
+
+  @Post('registrate')
+  async registrate(@Body() input: CreateUserDto) {
+    const hash = this.authService.hashPassword(input.password).
+    this.logger.debug(hash)
+    return 'SUCCESS'
+    // this.logg
+    // return await this.userRepository.save({
+    //   ...input,
+    //   password: hash,
+    // })
+  }
 
   @Post('login')
   @UseGuards(AuthGuard('local'))
-  async login(@Request() request) {
+  async login(@CurrentUser() user: User) {
     return {
-      userId: request.user.id,
-      token: this.authService.getTokenForUser(request.user),
+      userId: user.id,
+      token: this.authService.getTokenForUser(user),
     }
   }
 
   @Get('Profile')
   @UseGuards(AuthGuard('jwt'))
-  async getProfile(@Request() request) {
-    return request.user
+  async getProfile(@CurrentUser() user: User) {
+    return user
   }
 }
