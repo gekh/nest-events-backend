89b2244505d22bf4974e581ec58f0cd91487967f
German Khokhlov <gekh.oklo@gmail.com>
2021-11-18 17:00:13 +0300

    User and Profile entities

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Profile } from "./profile.entity";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+  @Column()
+  username: string
+  @Column()
+  password: string
+  @Column()
+  email: string
+  @Column()
+  firstName: string
+  @Column()
+  secondName: string
+  
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile
+}
